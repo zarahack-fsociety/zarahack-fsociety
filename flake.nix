3a4496d4db1ef3fe7a74ca33a9b2ef13f3cafdfd
simeon <simmeon.nv@proton.me>
2026-06-19 18:00:27 +0300

    flake init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    go-overlay = {
+      url = "github:purpleclay/go-overlay";
+      inputs = {nixpkgs.follows = "nixpkgs";};
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              # packages = [pkgs.hello];
+
+              languages.go = {
+                enable = true;
+                delve.enable = true;
+                lsp.enable = true;
+                version = "1.26.0";
+              };
+            }
+          ];
+        };
+      });
+  };
+}
